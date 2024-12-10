2c241be6cabf2ae05f8bdeb5bc3c90925c31a062
quocnd <>
2024-12-10 15:04:25 +0700

    circle clock

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,12 +1,32 @@
+using System.Timers;
 using Todo.ViewModels;
 
 namespace Todo.Pages;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage(HomePageViewModel viewModel)
+    private System.Timers.Timer timer;
+    public HomePage(HomePageViewModel viewModel)
 	{
 		InitializeComponent();
 		BindingContext = viewModel;
+
+        timer = new System.Timers.Timer();
+        timer.Interval = 1000;
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        // Calculate angles for hour and minute hands
+        double hourAngle = (DateTime.Now.Hour % 12 + DateTime.Now.Minute / 60.0) * 30;
+        double minuteAngle = DateTime.Now.Minute * 6;
+        double secondAngle = DateTime.Now.Second * 6;
+
+        // Update hand rotations
+        hourHand.Rotation = hourAngle;
+        minuteHand.Rotation = minuteAngle;
+        secondHand.Rotation = secondAngle;
     }
 }
